82d7d0f98db382ab467f760cbbfc1103c0822568
Ko Bhone <bhonem908@gmail.com>
2026-09-15 14:07:07 +0630

    feat: refix job description api endpoint

--- app/api/jobs.py
+++ app/api/jobs.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from app.schemas.job import JobDescriptionRequest
 from app.services.text_cleaner import clean_text
 from app.services.skill_extractor import extract_skills
@@ -6,28 +6,45 @@ from sqlalchemy.orm import Session
 from app.database.session import get_db
 from app.models.job import Job
 
+
 router = APIRouter()
 
 @router.post("/analyze", status_code=status.HTTP_200_OK)
 
-async def analyze_job(job: JobDescriptionRequest):
+async def analyze_job(job: JobDescriptionRequest, db: Session = Depends(get_db)):
     "Analyze job description and extract required skills"
 
+    #1. Get raw job description
     raw_text = job.job_description
+
+    #2. Clean text
     cleaned_text = clean_text(raw_text)
 
+    #3. Extract skills
     extracted_skills = extract_skills(cleaned_text)
 
+    #4. Create job Database object
+    new_job = Job(
+        job_description = raw_text,
+        cleaned_text = cleaned_text,
+        extracted_skills = extracted_skills
+    )
+
+    #5. Save to Postgresql
+    db.add(new_job)
+    db.commit()
+    db.refresh(new_job)
+
+    #6. Return Response
     return {
-        "message" : "Job description analyzed successfully",
+        "Message" : "Job Description analyzed successfully!",
+        "job_id" : new_job.id,
         "job_description" : raw_text,
         "cleaned_text" : cleaned_text,
-        "extracted_skills" : extracted_skills}
+        "extracted_skills" : extracted_skills
+    }
 
-@router.post("/analyze")
-def analyze_job(request : JobDescriptionRequest,
-                db: Session = Depends(get_db)):
-    return analyze_job(db=db)
+
 
 
 
