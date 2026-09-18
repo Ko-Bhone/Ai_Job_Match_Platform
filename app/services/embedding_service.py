adc364c11a47b7739d39abfa68e147989be3d65d
Ko Bhone <bhonem908@gmail.com>
2026-09-18 12:50:38 +0630

    feat: add sentence embedding service

---
+++ app/services/embedding_service.py
@@ -0,0 +1,13 @@
+from sentence_transformers import SentenceTransformer
+
+MODEL_NAME = "all-MiniLM-L6-v2"
+model = SentenceTransformer(MODEL_NAME)
+
+def create_embeddings(chunks : list[dict]) -> list[dict]:
+    texts = [chunk["content"] for chunk in chunks]
+    embeddings = model.encode(texts, convert_to_numpy=True)
+    embedded_chunks = []
+    for chunk, embedding in zip(chunks, embeddings):
+        embedded_chunks.append({**chunk, "embedding": embedding.tolist()})
+    return embedded_chunks
+
