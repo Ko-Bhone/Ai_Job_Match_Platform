1610b2520f2bc9e932319a0c8970c4d8153f0f06
Ko Bhone <bhonem908@gmail.com>
2026-09-25 14:57:23 +0630

    feat: Refix Api Routers

--- app/api/resume.py
+++ app/api/resume.py
@@ -2,52 +2,61 @@ from fastapi import APIRouter
 import shutil
 import uuid
 from pathlib import Path
-from fastapi import APIRouter, UploadFile, File, HTTPException, status
+from fastapi import APIRouter, UploadFile, File, HTTPException, status, Depends
 from app.services.resume_parser import extract_text_from_pdf
 from app.services.text_cleaner import clean_text
 from app.services.skill_extractor import extract_skills
+from sqlalchemy.orm import Session
+from app.database.session import get_db
+from app.models.resume import Resume
+
 
 router = APIRouter()
 UPLOAD_DIR = Path("uploads/resume")
 UPLOAD_DIR.mkdir(parents=True, exist_ok = True)
 
 @router.post("/upload", status_code=status.HTTP_201_CREATED)
+async def upload_resume(file: UploadFile = File(...), db:Session = Depends(get_db)):
 
-async def upload_resume(file: UploadFile = File(...)):
     #1.validate filename
     if not file.filename:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="File name is missing")
-
     #2. validate file type
     if file.content_type != "application/pdf":
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Only PDF files are allowed")
-
     #3.Create unique filename
     file_extension = Path(file.filename).suffix
     unique_filename = f"{uuid.uuid4()}{file_extension}"
-
     #4.Create file path
     file_path = UPLOAD_DIR / unique_filename
-
     #5.Save pdf
     with file_path.open("wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
-
     #6.Extract text
     extracted_text = extract_text_from_pdf(file_path)
-
     #7.Validate extracted text
     if not extracted_text.strip():
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Could not extract text form this pdf")
-
     #8.Clean text
     cleaned_text = clean_text(extracted_text)
-
     #9. Extract skills
     extracted_skills = extract_skills(cleaned_text)
 
+    resume = Resume(
+        original_filename=file.filename,
+        stored_filename=unique_filename,
+        content_type=file.content_type,
+        extracted_text=extracted_text,
+        cleaned_text=cleaned_text,
+        extracted_skills=extracted_skills)
+
+    db.add(resume)
+    db.commit()
+    db.refresh(resume)
+
     return {
         "Message": "Resume Uploaded Successfully",
+        "resume_id": resume.id,
         "original_filename": file.filename,
         "stored_filename": unique_filename,
         "content_type": file.content_type,
--- app/api/router.py
+++ app/api/router.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter
 from app.api import resume, jobs, matching, similarity, scoring, database, rag
-from app.api.jobs import router as jobs_router
 
 api_router = APIRouter()
 
-api_router.include_router(resume.router, prefix="/resume", tags=["Resumes"])
+api_router.include_router(resume.router, prefix="/resume", tags=["Resume"])
 
 api_router.include_router(jobs.router, prefix="/jobs", tags=["Jobs"])
 
@@ -12,8 +11,8 @@ api_router.include_router(matching.router, prefix="/matching", tags=["Matching"]
 
 api_router.include_router(similarity.router, prefix="/similarity", tags=["Similarity"])
 
-api_router.include_router(scoring.router, prefix="/score", tags=["Scoring"])
+api_router.include_router(scoring.router, prefix="/scoring", tags=["Scoring"])
 
 api_router.include_router(database.router, prefix="/database", tags=["Database"])
 
-api_router.include_router(rag.router, prefix="/rag", tags=["RAG recommendations"])
+api_router.include_router(rag.router, prefix="/rag", tags=["RAG"])
